099a1840ed3434e64be11d7581dd34728090761e
Gildas <1122076+djhi@users.noreply.github.com>
2025-04-24 10:59:11 +0200

    Add biome

--- src/server.tsx
+++ src/server.tsx
@@ -1,26 +1,26 @@
-import express from 'express';
-import {renderRequest, callAction} from '@parcel/rsc/node';
+import { callAction, renderRequest } from "@parcel/rsc/node";
+import express from "express";
 
 // Page components. These must have "use server-entry" so they are treated as code splitting entry points.
-import {Page} from './Page';
+import { Page } from "./Page";
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
-app.get('/', async (req, res) => {
-  await renderRequest(req, res, <Page />, {component: Page});
+app.get("/", async (req, res) => {
+	await renderRequest(req, res, <Page />, { component: Page });
 });
 
-app.post('/', async (req, res) => {
-  let id = req.get('rsc-action-id');
-  let {result} = await callAction(req, id);
-  let root: any = <Page />;
-  if (id) {
-    root = {result, root};
-  }
-  await renderRequest(req, res, root, {component: Page});
+app.post("/", async (req, res) => {
+	const id = req.get("rsc-action-id");
+	const { result } = await callAction(req, id);
+	let root: any = <Page />;
+	if (id) {
+		root = { result, root };
+	}
+	await renderRequest(req, res, root, { component: Page });
 });
 
 app.listen(3000);
-console.log('Server listening on port 3000');
+console.log("Server listening on port 3000");
