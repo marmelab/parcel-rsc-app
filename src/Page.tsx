4d800fab62f7f21bf90b35453bf19be3d5a7d38a
Gildas <1122076+djhi@users.noreply.github.com>
2025-04-24 11:16:29 +0200

    Add supabase

--- src/Page.tsx
+++ src/Page.tsx
@@ -2,30 +2,37 @@
 
 import "./page.css";
 import "./client";
-import { action, name } from "./actions";
+import { addTask, getTasks } from "./tasks";
 
 export async function Page() {
-	return (
-		<html lang="en">
-			<head>
-				<title>Parcel React Server App</title>
-			</head>
-			<body>
-				<h1>Parcel React Server App</h1>
-				<p>
-					This page is a React Server Component! Edit <code>src/Page.tsx</code>{" "}
-					to get started.
-				</p>
-				<hr />
-				<p>This form submits a server action.</p>
-				<form action={action}>
-					<label>
-						Enter your name: <input name="name" defaultValue={name} />
-					</label>
-					<button type="submit">Submit</button>
-				</form>
-				{name && <p>Welcome {name}!</p>}
-			</body>
-		</html>
-	);
+  const tasks = await getTasks();
+
+  return (
+    <html lang="en">
+      <head>
+        <title>Parcel React Server App</title>
+      </head>
+      <body>
+        <h1>Todo</h1>
+        {tasks.length === 0 ? (
+          <div>No tasks</div>
+        ) : (
+          <ul>
+            {tasks.map((task) => (
+              <li key={task.id}>
+                {task.description} {task.completed_at ? "✓" : "✗"}
+              </li>
+            ))}
+          </ul>
+        )}
+        <hr />
+        <form action={addTask}>
+          <label>
+            New task: <input name="name" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </body>
+    </html>
+  );
 }
---
+++ src/db/index.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./supabase.types";
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+    throw new Error(
+        "Missing SUPABASE_URL or SUPABASE_ANON_KEY environment variables in .env"
+    );
+}
+
+// Create a single supabase client for interacting with your database
+export const db = createClient<Database>(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
