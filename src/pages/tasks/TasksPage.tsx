d49d99bc7b1a6cf5fb3f1942fc84acbf2ee25cf8
Gildas <1122076+djhi@users.noreply.github.com>
2025-07-09 15:04:15 +0200

    Remove useless suspense

--- src/pages/tasks/TasksPage.tsx
+++ src/pages/tasks/TasksPage.tsx
@@ -1,26 +1,22 @@
 "use server-entry";
-import { Suspense } from "react";
 import { Layout } from "../../layout/Layout";
 import { TaskList } from "./TaskList";
-import { TaskListSkeleton } from "./TaskListSkeleton";
 import { getTasks } from "./tasks";
 
 export async function TasksPage({
-	filter,
+  filter,
 }: {
-	filter?: "active" | "completed" | undefined;
+  filter?: "active" | "completed" | undefined;
 }) {
-	const { tasks, totalActiveTasks } = await getTasks(filter);
+  const { tasks, totalActiveTasks } = await getTasks(filter);
 
-	return (
-		<Layout>
-			<Suspense fallback={<TaskListSkeleton />}>
-				<TaskList
-					filter={filter}
-					tasks={tasks}
-					totalActiveTasks={totalActiveTasks}
-				/>
-			</Suspense>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <TaskList
+        filter={filter}
+        tasks={tasks}
+        totalActiveTasks={totalActiveTasks}
+      />
+    </Layout>
+  );
 }
