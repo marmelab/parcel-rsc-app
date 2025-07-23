4c0e859bce803c2fc049136880637ce388b9fe14
Gildas <1122076+djhi@users.noreply.github.com>
2025-07-23 15:08:23 +0200

    Improve TaskFilters

--- src/pages/tasks/TaskFilters.tsx
+++ src/pages/tasks/TaskFilters.tsx
@@ -1,32 +1,36 @@
 export const TaskFilters = ({
-	filter,
+  filter,
 }: {
-	filter: "active" | "completed" | undefined;
+  filter: "active" | "completed" | undefined;
 }) => (
-	<div className="join w-full">
-		<a
-			href="/"
-			data-active={filter == null ? true : undefined}
-			className="basis-1/3 group btn btn-sm join-item data-active:btn-primary"
-		>
-			<span className="hidden group-data-loading:block group-data-loading:loading group-data-loading:loading-spinner group-data-loading:loading-xs" />
-			All
-		</a>
-		<a
-			href="/active"
-			data-active={filter === "active" ? true : undefined}
-			className="basis-1/3 group btn btn-sm join-item data-active:btn-primary"
-		>
-			<span className="hidden group-data-loading:block group-data-loading:loading group-data-loading:loading-spinner group-data-loading:loading-xs" />
-			Active
-		</a>
-		<a
-			href="/completed"
-			data-active={filter === "completed" ? true : undefined}
-			className="basis-1/3 group btn btn-sm join-item data-active:btn-primary"
-		>
-			<span className="hidden group-data-loading:block group-data-loading:loading group-data-loading:loading-spinner group-data-loading:loading-xs" />
-			Completed
-		</a>
-	</div>
+  <div className="join w-full">
+    <TaskFilter currentFilter={filter}>
+      All
+    </TaskFilter>
+    <TaskFilter filter="active" currentFilter={filter}>
+      Active
+    </TaskFilter>
+    <TaskFilter filter="completed" currentFilter={filter}>
+      Completed
+    </TaskFilter>
+  </div>
+);
+
+const TaskFilter = ({
+  currentFilter,
+  filter,
+  children,
+}: {
+  currentFilter: "active" | "completed" | undefined;
+  filter?: "active" | "completed";
+  children: React.ReactNode;
+}) => (
+  <a
+    href={`/${filter}`}
+    data-active={currentFilter === filter ? true : undefined}
+    className="basis-1/3 group btn btn-sm join-item data-active:btn-primary"
+  >
+    <span className="hidden group-data-loading:block group-data-loading:loading group-data-loading:loading-spinner group-data-loading:loading-xs" />
+    {children}
+  </a>
 );
